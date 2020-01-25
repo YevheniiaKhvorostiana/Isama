7441bd2d040fe9541770e2c65c2936f4c9b166af
Yevheniia Khvorostiana <yevheniia.khvorostiana@gmail.com>
2020-01-25 21:29:23 +0200

    initial commit

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(".js-slider").slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  centerPadding: '0px',
+  variableWidth: true,
+  prevArrow: '<button class="slider-arrow slider-arrow--prev"><div class="slider-left"></div></button>',
+  nextArrow: '<button class="slider-arrow slider-arrow--next"><div class="slider-right"></div></button>'
+  
+});
